ce6f9e5f11656bfe5f6538234ccce819861b9781
lcarlso2 <lcarlso2@my.westga.edu>
2020-01-27 21:19:39 -0500

    Added comments to all files. Added functionality to prevent crashing when user signs out and tries to rent an item.

--- code/RentMe/RentMe/Models/Customer.cs
+++ code/RentMe/RentMe/Models/Customer.cs
@@ -3,12 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RentMe.Models
 {
+	/// <summary>
+	/// The class responsible for keeping track of customer object
+	/// </summary>
 	public class Customer
 	{
+		/// <summary>
+		/// Gets or sets the email of the customer
+		/// </summary>
 		[Required]
 		[DataType(DataType.EmailAddress)]
 		public string Email { get; set; }
 
+		/// <summary>
+		/// Gets or set the password
+		/// </summary>
 		[Required]
 		[DataType(DataType.Password)]
 		public string Password { get; set; }
--- code/RentMe/RentMe/Models/MediaModel.cs
+++ code/RentMe/RentMe/Models/MediaModel.cs
@@ -5,15 +5,30 @@ using System.Threading.Tasks;
 
 namespace RentMe.Models
 {
+    /// <summary>
+    /// The class responsible for keeping track of the media
+    /// </summary>
     public class MediaModel
     {
+        /// <summary>
+        /// Gets or sets the id
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the title
+        /// </summary>
         public string Title { get; set; }
 
+
+        /// <summary>
+        /// Gets or sets the category
+        /// </summary>
         public string Category { get; set; }
 
-
+        /// <summary>
+        /// The constructor for the media
+        /// </summary>
         public MediaModel()
         {
 
