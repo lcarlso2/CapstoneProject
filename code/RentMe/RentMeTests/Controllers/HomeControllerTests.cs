1dfe3aa49b5f385fd0ddcd0bee3e1b7dcf427b5b
lcarlso2 <lcarlso2@my.westga.edu>
2020-02-03 16:50:03 -0500

    Added a test project. Sees tests, but they still don't run

---
+++ code/RentMe/RentMeTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RentMe.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentMeTests.Controllers.Tests
+{
+	[TestClass()]
+	public class HomeControllerTests
+	{
+		[TestMethod()]
+		public void IndexTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void ConfirmBorrowTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		public void TestTest()
+		{
+			Assert.AreEqual(1,1);
+		}
+	}
+}
