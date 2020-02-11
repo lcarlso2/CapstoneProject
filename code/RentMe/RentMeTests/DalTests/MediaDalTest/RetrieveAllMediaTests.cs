c8be79124bac1f2c17ed4f10c939c84088c864e3
lcarlso2 <lcarlso2@my.westga.edu>
2020-02-11 11:25:42 -0500

    Added tests for add employee in the employee dal

--- code/RentMe/RentMeTests/DalTests/MediaDalTest/RetrieveAllMediaTests.cs
+++ code/RentMe/RentMeTests/DalTests/MediaDalTest/RetrieveAllMediaTests.cs
@@ -19,7 +19,7 @@ namespace RentMeTests.SharedCode.MediaDalTest
 
 			var result = mediaDal.RetrieveAllMedia();
 
-			Assert.AreEqual(6, result.Count);
+			Assert.AreEqual(5, result.Count);
 		}
 	}
 }
---
+++ code/RentMe/RentMeTests/SharedCodeTests/DalTests/EmployeeDalTests/AddEmployeeTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedCode.DAL;
+using SharedCode.Model;
+
+namespace RentMeTests.SharedCodeTests.DalTests.EmployeeDalTests
+{
+	/// <summary>
+	/// The test class for add employee
+	/// </summary>
+	[TestClass()]
+	public class AddEmployeeTests
+	{
+		[TestMethod()]
+		public void AddEmployeeValidTest()
+		{
+			var employeeDal = new EmployeeDal();
+			var employee = new Employee
+			{
+				FirstName = "TestEmployeeForTesting",
+				LastName = "TestEmployeeForTesting",
+				Username = "TestUsernameForTest",
+				IsManager = false,
+				Password = "testpasswordfortesting"
+			};
+			employeeDal.AddEmployee(employee, "testpasswordfortesting");
+
+			var result = employeeDal.Authenticate(employee.Username, employee.Password);
+
+			Assert.AreEqual(1, result);
+
+			employeeDal.RemoveEmployee(employee.Username);
+
+			var resultAfterDDelete = employeeDal.Authenticate(employee.Username, employee.Password);
+
+			Assert.AreEqual(0, resultAfterDDelete);
+
+		}
+
+	}
+}
