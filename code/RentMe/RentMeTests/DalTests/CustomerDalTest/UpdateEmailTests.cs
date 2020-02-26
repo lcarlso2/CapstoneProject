aa300fe9bab7cb1ade33c05147a32b3cea57b443
Tristen Rivera <trivera1@my.westga.edu>
2020-02-25 23:37:23 -0500

    Added Code coverage report Added the resharper code coverage report Added the update email dal test for rentme (member)

---
+++ code/RentMe/RentMeTests/DalTests/CustomerDalTest/UpdateEmailTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using RentMe.DAL;
+using RentMe.Models;
+using SharedCode.DAL;
+
+namespace RentMeTests.DalTests.CustomerDalTest
+{
+
+    /// <summary>
+    /// The test class for updating an email for a customer 
+    /// </summary>
+    [TestClass()]
+    public class UpdateEmailTests
+    {
+        [TestMethod()]
+        public void UpdateEmail()
+        {
+            var customer = new Customer { Email = "email@email.com" };
+            var testEmail = "test@test.com";
+            var customerDal = new CustomerDal();
+
+            Assert.AreEqual(1, customerDal.Authenticate(customer.Email, "Password"));
+
+            customerDal.UpdateEmail(customer.Email, testEmail);
+
+            Assert.AreEqual(1, customerDal.Authenticate(testEmail, "Password"));
+
+            customerDal.UpdateEmail(testEmail, customer.Email);
+        }
+
+    }
+}
+
